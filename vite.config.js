885aa1b437d03e5a004153f6e99dd76adb51adec
Sultepronit <sultepronit@gmail.com>
2025-02-27 14:01:51 +0200

    upgraded to PWA

---
+++ vite.config.js
@@ -0,0 +1,37 @@
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import manifest from './manifest.json';
+
+export default defineConfig({
+    plugins: [
+        VitePWA({
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon*.png'],
+            manifest,
+            workbox: {
+                runtimeCaching: [
+                    { // articles
+                        urlPattern: ({ url }) => {
+                            return url.searchParams.get('dic')
+                                || url.pathname.includes('/fetchWebsiteContent');
+                        },
+                        handler: 'CacheFirst',
+                        options: {
+                            cacheName: 'articles',
+                            expiration: {
+                                maxEntries: 200
+                            }
+                        }
+                    },
+                    { // json-lib
+                        urlPattern: ({ url }) => url.pathname.endsWith('.json'),
+                        handler: 'CacheFirst',
+                        options: {
+                            cacheName: 'jsonLib'
+                        }
+                    }
+                ]
+            }
+        })
+    ]
+});
